6bc8a99ed69d82e285298121503dcb581bd8ade0
Engineer Saima <engrsaimakousar@gmail.com>
2026-02-22 20:55:55 +0500

    Add files via upload

---
+++ createFile.py
@@ -0,0 +1,21 @@
+# 1- Creating the file in computer using write (w) mode
+file = open("First.py", "w")
+file.write("This file is created.")
+file.close()
+
+file = open("First.txt", "w")
+file.write("This file is created.")
+file.close()
+
+
+# 2- Creating file in computer using with keyword
+with open("file.txt", "w") as file:
+    file.write("This is pyhon text file created using with keywords.")
+
+#Creating file in computer using OS module
+import os
+
+# 3- Define specific path
+file_path = "C:\\Users\\YourName\\Desktop\\new_file.txt"
+with open("file_path", "w") as file:
+    file.write("This file is created using OS Module")
