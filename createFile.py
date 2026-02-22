99a4602aa9583d52e0dcf5dc5af0f2d29db94aac
Engineer Saima <engrsaimakousar@gmail.com>
2026-02-22 21:04:03 +0500

    Update createFile.py

--- createFile.py
+++ createFile.py
@@ -11,11 +11,18 @@ file.close()
 # 2- Creating file in computer using with keyword
 with open("file.txt", "w") as file:
     file.write("This is pyhon text file created using with keywords.")
+    
 
-#Creating file in computer using OS module
+# 3- Creating file in computer using OS module
 import os
 
-# 3- Define specific path
+#Define specific path
 file_path = "C:\\Users\\YourName\\Desktop\\new_file.txt"
 with open("file_path", "w") as file:
+
     file.write("This file is created using OS Module")
+
+
+# Append the data into the file
+with open("file.txt", "a") as file:
+    file.write("This is appended data in the file using append a mode") 
